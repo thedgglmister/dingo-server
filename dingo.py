6948aea8c8bcd5dd298103e4c361aa7060813fe0
snookieordie <BenIremonger@Bens-MacBook-Pro.local>
2018-01-26 14:15:08 -0800

    dasdasf

--- dingo.py
+++ dingo.py
@@ -175,7 +175,7 @@ def game(username, game_name):
 
 	conn = db_connect()
 	c = conn.cursor()
-	c.execute("""SELECT slot, cards.breed, filename FROM cards JOIN dogs ON cards.breed = dogs.breed FROM cards JOIN games ON cards.game_id = games.id WHERE username = %s AND game_name = %s""", (username, game_name))
+	c.execute("""SELECT slot, cards.breed, filename FROM cards, dogs, games WHERE cards.breed = dogs.breed AND cards.game_id = games.game_id AND username = %s AND game_name = %s""", (username, game_name))
 
 	card = c.fetchall().sort()
 
