e56f7ad9e9c7e5a763a041c9c2788a31f04ed3ab
snookieordie <BenIremonger@Bens-MacBook-Pro.local>
2018-02-17 15:58:05 -0800

    dsada

--- dingo.py
+++ dingo.py
@@ -27,7 +27,7 @@ CARD_SIZE = 4
 
 #######
 def db_connect():
-	if __name__ == "__main__": #for testing
+	if len(argv) == 2: #for testing
 		conn = connect("dbname=dingo")
 	else:
 		parse.uses_netloc.append("postgres")
@@ -248,7 +248,8 @@ def create_card(game_name):
 		conn.commit()
 		conn.close()
 		return redirect(url_for("game", username=username, game_name=game_name))
-
+	else:
+		redirect(url_for("login"))
 
 
 
