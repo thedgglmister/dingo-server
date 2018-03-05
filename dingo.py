a8fff1113212045e6e8c45d91fdded83720fa9e9
snookieordie <BenIremonger@Bens-MacBook-Pro.local>
2018-03-05 13:36:08 -0800

    starting ios

--- dingo.py
+++ dingo.py
@@ -352,9 +352,9 @@ def handle_request(requester, confirm):
 	conn.close()
 	return redirect(url_for('user', username=username))
 
-@app.rout("/ios-test")
-def ios-test():
-	return "ios-test worked"
+@app.rout("/ios_test")
+def ios_test():
+	return "ios_test worked"
 
 
 
