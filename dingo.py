f688a4056306bcb81b43168fb04452aeccbd50ad
snookieordie <BenIremonger@Bens-MacBook-Pro.local>
2018-04-25 19:06:21 -0700

    Starting gettig things going with react

--- dingo.py
+++ dingo.py
@@ -9,7 +9,7 @@ from breed_classifier.inference.classify import infer
 from breed_classifier.common import consts
 from urllib import parse
 from sys import argv
-from flask_cors import CORS ##need this? uninstall?
+from flask_cors import CORS ##need this? uninstall? deal with preflighting manually to set allow-origin? this is safer?
 
 
 ###everything needs to get redirected if not logged in etc.
@@ -413,13 +413,13 @@ def validate_breed(): ## give infer image without saving?
 
 
 
-@app.route("/signup", methods=["POST", ])# "OPTIONS"])   
+@app.route("/signup", methods=["POST", "OPTIONS"])   
 def signup():
-#	if request.method == "OPTIONS":
-#		response = Response()
-#		response.headers['Access-Control-Allow-Origin'] = "*"
-#		response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-#		return response
+	if request.method == "OPTIONS":
+		response = Response()
+		response.headers['Access-Control-Allow-Origin'] = "*"
+		response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+		return response
 	print(1)
 	request_data = request.get_json()
 	email = request_data.get('emailAddress')
