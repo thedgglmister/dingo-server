8ae16d123e82de54457ee0ff463f23f878c2ac3c
snookieordie <BenIremonger@Bens-MacBook-Pro.local>
2018-05-03 16:48:28 -0700

    notificiations

--- dingo.py
+++ dingo.py
@@ -758,8 +758,8 @@ def leave_game():
 	curs.execute("""UPDATE gameplayers SET in_game = FALSE WHERE gameplayer_id = %s;""", (gpid,))
 	conn.commit()
 
-	curs.execute("""INSERT INTO notifications (gameplayer_id, notifier_id, type) SELECT gameplayer_id, %s, %s FROM gameplayers WHERE game_id = %s AND user_id != %s;""", (user_id, 'leave', game_id, user_id))
-	conn.commit()
+	#curs.execute("""INSERT INTO notifications (gameplayer_id, notifier_id, type) SELECT gameplayer_id, %s, %s FROM gameplayers WHERE game_id = %s AND user_id != %s;""", (user_id, 'leave', game_id, user_id))
+	#conn.commit()
 
 	conn.close()
 	curs.execute("""INSERT INTO notifications (gameplayer_id, notifier_id, type) SELECT gameplayer_id, %s, %s FROM gameplayers WHERE game_id = %s AND user_id != %s;""", (user_id, 'join', game_id, user_id))
